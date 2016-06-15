1a78820405ee5da1dc8388d1d7e2ec32ecf146fe
CubePro <cubeprofessional@gmail.com>
2016-06-15 17:22:27 -0500

    styled 404 page

--- 404.php
+++ 404.php
@@ -7,58 +7,66 @@
  * @package Anti-Flash-White
  */
 
-get_header(); ?>
+get_header();
+include('left-sidebar.php');
+?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+		<div id="primary" class="content-area error">
+			<main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'Anti-Flash-White' ); ?></h1>
-				</header><!-- .page-header -->
+				<section class="error-404 not-found">
+					<header class="page-header">
+						<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'Anti-Flash-White' ); ?></h1>
+					</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'Anti-Flash-White' ); ?></p>
+					<div class="page-content">
+						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'Anti-Flash-White' ); ?></p>
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( anti_flash_white_theme_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'Anti-Flash-White' ); ?></h2>
-						<ul>
 						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
+							get_search_form();
+
+							the_widget( 'WP_Widget_Recent_Posts' );
+
+							// Only show the widget if site has multiple categories.
+							if ( anti_flash_white_theme_categorized_blog() ) :
 						?>
-						</ul>
-					</div><!-- .widget -->
 
-					<?php
-						endif;
+						<div class="widget widget_categories">
+							<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'Anti-Flash-White' ); ?></h2>
+							<ul>
+							<?php
+								wp_list_categories( array(
+									'orderby'    => 'count',
+									'order'      => 'DESC',
+									'show_count' => 1,
+									'title_li'   => '',
+									'number'     => 10,
+								) );
+							?>
+							</ul>
+						</div><!-- .widget -->
 
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'Anti-Flash-White' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+						<?php
+							endif;
 
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+							/* translators: %1$s: smiley */
+							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'Anti-Flash-White' ), convert_smilies( ':)' ) ) . '</p>';
+							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+							the_widget( 'WP_Widget_Tag_Cloud' );
+						?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					</div><!-- .page-content -->
+				</section><!-- .error-404 -->
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div>
 
 <?php
+get_sidebar();
 get_footer();
+?>
+
+
